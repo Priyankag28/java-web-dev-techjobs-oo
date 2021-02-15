993e9db3328d2915d38a33e3c5f62f125d94989c
priyanka <priyanakangel28@gmail.com>
2021-02-15 17:13:58 -0600

    Assignment-2

--- src/org/launchcode/techjobs_oo/JobField.java
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -27,7 +27,7 @@ private String value;
         this.value = value;
     }
     public String toString() {
-        if(value ==" "){
+        if(value == null || value == ""){
             return "Data not available";
         }else return value;
     }
